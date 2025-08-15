06153a8643da2a7d15e0cb52032e90f094d94359
haseebm139 <haseebm139@gmail.com>
2025-08-16 04:46:40 +0500

    remove

--- app/Http/Controllers/Api/V1/Chat/ChatController.php
+++ app/Http/Controllers/Api/V1/Chat/ChatController.php
@@ -15,33 +15,33 @@ class ChatController  extends BaseController
 {
     public function startChat(Request $request)
     {
+        $this->validate($request, [
+            'receiver_id' => 'required|integer|exists:users,id',
+            'chat_type' => 'required|in:user_artist,artist_studio',
+        ]);
+        $validator = Validator::make($request->all(), [
+            'receiver_id' => 'required|integer|exists:users,id',
+            'chat_type' => 'required|in:user_artist,artist_studio',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError($validator->errors()->first(),$errorMessages = [], 422);
+        }
+
+        $chat = Chat::firstOrCreate([
+            'sender_id' => auth()->id(),
+            'receiver_id' => $request->receiver_id,
+            'chat_type' => $request->chat_type,
+        ], []);
+
+        broadcast(new NewChatStarted($chat))->toOthers();
+        if ($chat->wasRecentlyCreated) {
+
+        }
+
+        return $this->sendResponse($chat, 'Start Chat', 201);
         try {
 
-            $this->validate($request, [
-                'receiver_id' => 'required|integer|exists:users,id',
-                'chat_type' => 'required|in:user_artist,artist_studio',
-            ]);
-            $validator = Validator::make($request->all(), [
-                'receiver_id' => 'required|integer|exists:users,id',
-                'chat_type' => 'required|in:user_artist,artist_studio',
-            ]);
-
-            if ($validator->fails()) {
-                return $this->sendError($validator->errors()->first(),$errorMessages = [], 422);
-            }
-
-            $chat = Chat::firstOrCreate([
-                'sender_id' => auth()->id(),
-                'receiver_id' => $request->receiver_id,
-                'chat_type' => $request->chat_type,
-            ], []);
-
-            broadcast(new NewChatStarted($chat))->toOthers();
-            if ($chat->wasRecentlyCreated) {
-
-            }
-
-            return $this->sendResponse($chat, 'Start Chat', 201);
         } catch (\Throwable $th) {
             return $this->sendError('Failed to start chat');
         }
@@ -49,22 +49,22 @@ class ChatController  extends BaseController
 
     public function index()
     {
+        $userId = auth()->id();
+
+        $chat = Chat::where('sender_id', $userId)
+        ->orWhere('receiver_id', $userId)
+        ->with(['lastMessage', 'receiver', 'sender'])
+        ->orderByDesc(
+            Message::select('created_at')
+                ->whereColumn('chat_id', 'chats.id')
+                ->latest()
+                ->take(1)
+        )
+        ->get();
+
+        return $this->sendResponse($chat, 'Chats Fetched');
         try {
             //code...
-            $userId = auth()->id();
-
-            $chat = Chat::where('sender_id', $userId)
-            ->orWhere('receiver_id', $userId)
-            ->with(['lastMessage', 'receiver', 'sender'])
-            ->orderByDesc(
-                Message::select('created_at')
-                    ->whereColumn('chat_id', 'chats.id')
-                    ->latest()
-                    ->take(1)
-            )
-            ->get();
-
-            return $this->sendResponse($chat, 'Chats Fetched');
         } catch (\Throwable $th) {
             return $this->sendError('Failed to fetch chats');
         }
