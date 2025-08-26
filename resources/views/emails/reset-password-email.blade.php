ea52022bb8237a7471e8fa95301ef6b5ee95488d
haseebm139 <haseebm139@gmail.com>
2025-08-27 04:18:10 +0500

    add email

--- resources/views/emails/reset-password-email.blade.php
+++ resources/views/emails/reset-password-email.blade.php
@@ -1,12 +1,20 @@
-
 <!DOCTYPE html>
 <html>
 
 <head>
     <title></title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta
+        http-equiv="Content-Type"
+        content="text/html; charset=utf-8"
+    />
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+    >
+    <meta
+        http-equiv="X-UA-Compatible"
+        content="IE=edge"
+    />
     <style type="text/css">
         @media screen {
             @font-face {
@@ -108,44 +116,111 @@
         style="display: none; font-size: 1px; color: #e5f8f2; line-height: 1px; font-family: 'Lato', Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;">
         We're thrilled to have you here! Get ready to dive into your new account.
     </div>
-    <table border="0" cellpadding="0" cellspacing="0" width="100%">
+    <table
+        border="0"
+        cellpadding="0"
+        cellspacing="0"
+        width="100%"
+    >
         <!-- LOGO -->
         <tr>
-            <td bgcolor="#e5f8f2" align="center">
-                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+            <td
+                bgcolor="#e5f8f2"
+                align="center"
+            >
+                <table
+                    border="0"
+                    cellpadding="0"
+                    cellspacing="0"
+                    width="100%"
+                    style="max-width: 600px;"
+                >
                     <tr>
-                        <td align="center" valign="top" style="padding: 40px 10px 40px 10px;"> </td>
+                        <td
+                            align="center"
+                            valign="top"
+                            style="padding: 40px 10px 40px 10px;"
+                        > </td>
                     </tr>
                 </table>
             </td>
         </tr>
         <tr>
-            <td bgcolor="#e5f8f2" align="center" style="padding: 0px 10px 0px 10px;">
-                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+            <td
+                bgcolor="#e5f8f2"
+                align="center"
+                style="padding: 0px 10px 0px 10px;"
+            >
+                <table
+                    border="0"
+                    cellpadding="0"
+                    cellspacing="0"
+                    width="100%"
+                    style="max-width: 600px;"
+                >
                     <tr>
-                        <td bgcolor="#e5f8f2" align="center" valign="top"
-                            style="padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #fefefe; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;">
-                            <h1 style="font-size: 48px; font-weight: 400; margin: 2; color: #014122;">Verification Code</h1> <img
-                                src="{{asset('assets/media/logos/custom-1.png')}}" width="125" height="120"
-                                style="display: block; border: 0px;" />
+                        <td
+                            bgcolor="#e5f8f2"
+                            align="center"
+                            valign="top"
+                            style="padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #fefefe; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;"
+                        >
+                            <h1 style="font-size: 48px; font-weight: 400; margin: 2; color: #014122;">Verification Code
+                            </h1> <img
+                                src="{{ asset('assets/media/logos/custom-1.png') }}"
+                                width="125"
+                                height="120"
+                                style="display: block; border: 0px;"
+                            />
                         </td>
                     </tr>
                 </table>
             </td>
         </tr>
         <tr>
-            <td bgcolor="#e5f8f2" align="center" style="padding: 0px 10px 0px 10px;">
-                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+            <td
+                bgcolor="#e5f8f2"
+                align="center"
+                style="padding: 0px 10px 0px 10px;"
+            >
+                <table
+                    border="0"
+                    cellpadding="0"
+                    cellspacing="0"
+                    width="100%"
+                    style="max-width: 600px;"
+                >
                     <tr>
-                        <td bgcolor="#e5f8f2" align="left">
-                            <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                        <td
+                            bgcolor="#e5f8f2"
+                            align="left"
+                        >
+                            <table
+                                width="100%"
+                                border="0"
+                                cellspacing="0"
+                                cellpadding="0"
+                            >
                                 <tr>
-                                    <td bgcolor="#e5f8f2" align="center" style="padding: 20px 30px 60px 30px;">
-                                        <table border="0" cellspacing="0" cellpadding="0">
+                                    <td
+                                        bgcolor="#e5f8f2"
+                                        align="center"
+                                        style="padding: 20px 30px 60px 30px;"
+                                    >
+                                        <table
+                                            border="0"
+                                            cellspacing="0"
+                                            cellpadding="0"
+                                        >
                                             <tr>
-                                                <td align="center" style="border-radius: 3px;" bgcolor="#e5f8f2"><a
+                                                <td
+                                                    align="center"
+                                                    style="border-radius: 3px;"
+                                                    bgcolor="#e5f8f2"
+                                                ><a
                                                         href="javascript::void(0)"
-                                                        style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color:#014122; text-decoration: none; color: #014122; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid #014122; display: inline-block;">Your
+                                                        style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color:#014122; text-decoration: none; color: #014122; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid #014122; display: inline-block;"
+                                                    >Your
                                                         Verification Code is:
                                                         <?= $otp ?>
                                                     </a></td>
